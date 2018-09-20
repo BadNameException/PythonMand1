9ed4ef15df37cc1c285891a44f481ad83fd4deb8
root <root@localhost.localdomain>
2018-09-20 19:27:49 -0400

    Snorre as name cause im scared

--- Task3.py
+++ Task3.py
@@ -1,34 +1,33 @@
-password_list = {"chris":"helloworld", "john":"passw1", "nelly":"2hell1", "wendy":"1Passw"}
- 
+password_list = {"chris": "helloworld", "john": "passw1", "nelly": "2hell1", "wendy": "1Passw"}
+
+
 def pwfinder(pwlist):
- 
-    global hasUpper
-    global hasDigit
-    global is6Digit
+
     for value in pwlist.values():
-        hasDigit = False
-        hasUpper = False
-        is6Digit = False
- 
-        if len(value) < 6:
-            is6Digit = False
-        elif len(value) > 6:
-            is6Digit = False
+
+        print(len(value))
+        if not len(value) == 6:
+
+            password_array = [password_list[value]]
+
+            # Check if the first character is a digit.
+            if password_array[0].isdigit():
+                for index, character in enumerate(password_array, start=1):
+                    print(character)
+                    if character.isupper():
+                        has_upper = True
+                        break
+                    else:
+                        # Print out a message showing the program came to this point.
+                        # This could be bad practice in a real program as it would print
+                        # out all the password not matching the criteria.
+                        print("The password \" {} \" has no uppercast letters".format())
+
+            if has_upper is True:
+                print("The password " + value + " matches your criteria!")
+                has_upper = False
         else:
-            is6Digit = True
- 
-        valuearray = list(value)
-        for c in valuearray:
-            if c.isdigit():
-                hasDigit = True
-            elif c.isupper():
-                hasUpper = True
- 
-        if hasUpper is True and hasDigit is True and is6Digit is True:
-            print("The password " + value + " matches your criteria!")
-            hasDigit = False
-            hasUpper = False
-            is6Digit = False
- 
- 
+            print("Password does not have {} characters")
+
+
 pwfinder(password_list)
