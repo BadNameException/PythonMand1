db8cd9ec8bf117318dd3d96ea01535929e47ff3f
sannitie <root@localhost.localdomain>
2018-09-20 20:10:44 -0400

    Shortened and done

--- Task3.py
+++ Task3.py
@@ -1,32 +1,20 @@
-password_list = {"chris": "helloworld", "john": "passw1", "nelly": "2hell1", "wendy": "1Passw"}
+password_list = {"chris": "helloworld", "john": "passw1", "nelly": "2hell1", "wendy": "1Passw", "hei": "dinsei"}
 
 
 def pwfinder(pwlist, length):
 
-    global has_upper
-
     for value in pwlist.values():
-        print(value)
+
         # Check if the password has length-parameter amounts of characters.
         if len(value) == length:
-
-
             password_array = list(value)
 
             # Check if the first character is a digit.
             if password_array[0].isdigit():
-                #for index, character in enumerate(password_array, start=1):
-                for character in password_array:
-                    print(character)
+                for index, character in enumerate(password_array, start=1):
                     if character.isupper():
-                        has_upper = True
+                        print("The password " + value + " matches your criteria!")
                         break
-
-                if has_upper is True:
-                    print("The password " + value + " matches your criteria!")
-                    has_upper = False
-        else:
-            print("The password \"{}\" does not have {} characters.".format(value, length))
 
 
 pwfinder(password_list, 6)
